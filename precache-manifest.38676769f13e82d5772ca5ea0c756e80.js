eb5140e38cd95f8b5deb59d7bf5e554aabd5ece4
Jesús Macías Félix <37628262+jmaciasfelix@users.noreply.github.com>
2020-08-07 14:33:10 +0200

    Add files via upload

---
+++ precache-manifest.38676769f13e82d5772ca5ea0c756e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc3b6919347def541c839056db8a611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c791b39433765c8fd49b",
+    "url": "/static/css/main.16216c84.chunk.css"
+  },
+  {
+    "revision": "519bb7b1d2177ee5f3ac",
+    "url": "/static/js/2.ca8e7ab0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca8e7ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c791b39433765c8fd49b",
+    "url": "/static/js/main.56ba5a04.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
